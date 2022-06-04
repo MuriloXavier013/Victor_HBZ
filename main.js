b55815ce4563e3f552c219d4060382793034c044
MuriloXavier013 <muriloxavier1001@gmail.com>
2022-06-03 21:59:06 -0300

    Primeiro commit

---
+++ main.js
@@ -0,0 +1,126 @@
+/* Abre e fecha o menu ao cliclar no icone: hamburguer*/
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em um item do menu, esconder o menu*/
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar a cor do header ao scrollar*/
+
+const header = document.querySelector('#header')
+const navheight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navheight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+/* mudar a cor do title a ao scrollar*/
+
+// function changeTitleWhenScroll() {
+//   const title = document.querySelector('.title')
+//   const navheight = header.offsetHeight
+
+//   if (window.scrollY < navheight) {
+//     getElementByClassName('.title').classList.remove('scroll')
+//   } else {
+//     getElementByClassName('.title').classList.add('scroll')
+//   }
+// }
+
+// Testimonials carousel slider swiper
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+// ScrollReveal
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text, 
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  footer .brand, footer .social`,
+  { interval: 100 }
+)
+
+// Back to Top Button
+
+const backToTopButton = document.querySelector('.back-to-top')
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+const sections = document.querySelectorAll('main section(id')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+// When Scroll
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  changeTitleWhenScroll()
+  activateMenuAtCurrentSection()
+})
